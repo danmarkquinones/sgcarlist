5ff98e58f0ed38a3bea2e1f8e224a83117d11acf
danmark.quinones <danmark.quinones@pointwest.com.ph>
2021-11-03 22:52:27 +0800

    2nd commit

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView >
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        >
+        <Text>SGCARLIST</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
