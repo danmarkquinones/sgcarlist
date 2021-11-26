df14ab2dce5c0d7e31e306b168183dc2054228db
Mark Kristian Oco <mark.oco.ext@axa.com.ph>
2021-11-27 01:14:41 +0800

    update register

--- components/screens/auth_page/Register.js
+++ components/screens/auth_page/Register.js
@@ -5,18 +5,20 @@ import {theme} from '../../contants/colors';
 import logo from '../../../assets/images/sgcarlist_logo.png';
 import PrimaryInput from '../../custom_components/customInput';
 import Spacer from '../../custom_components/spacer';
+import {PrimaryButton} from '../../custom_components/customButtons';
 
 const Register = props => {
   const {width, height} = Dimensions.get('screen');
   const {navigation} = props;
   return (
-    <ScrollView style={{flex: 1}}>
-      <View style={{flex: 1, padding: 24}}>
+    <ScrollView contentContainerStyle={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <Spacer bottom={24} />
         <View
           style={{
             position: 'absolute',
             width: width,
-            height: height / 2,
+            height: height,
           }}>
           <View
             style={{
@@ -38,62 +40,61 @@ const Register = props => {
           />
         </View>
 
-        <View
-          style={{
-            position: 'absolute',
-            height: height / 2,
-            width: width * 2.5,
-            backgroundColor: theme.lightBlue,
-            bottom: -150,
-            left: -65,
-            transform: [{rotate: '15deg'}],
-            zIndex: 10,
-          }}
-        />
-
         <View style={{zIndex: 10}}>
           <View
             style={{
               padding: 24,
               backgroundColor: 'rgba(255, 255, 255 ,.8)',
-              borderRadius: 8,
+              justifyContent: 'center',
+              alignItems: 'center',
             }}>
             <Image
               source={logo}
               style={{
-                width: width - 100,
-                height: 80,
+                width: '90%',
+                height: 50,
+                resizeMode: 'contain',
               }}
             />
           </View>
           <Spacer bottom={32} />
-          <PrimaryInput placeholder="First Name" />
-          <Spacer bottom={24} />
-          <PrimaryInput placeholder="Last Name" />
-          <Spacer bottom={24} />
-          <PrimaryInput placeholder="Email" />
-          <Spacer bottom={24} />
-          <PrimaryInput placeholder="Contact number" />
-          <Spacer bottom={24} />
-          <PrimaryInput placeholder="Password" />
-          <Spacer bottom={24} />
-          <PrimaryInput placeholder="Confirm password" />
-          <Spacer bottom={24} />
-          <Button
-            color={theme.primaryBlue}
-            onPress={() => navigation.navigate('Login')}
-            title="Sign Up"
-          />
-          <Spacer bottom={24} />
-          <Text style={{textAlign: 'center'}}>
-            Already have an account?{' '}
-            <Text
-              onPress={() => navigation.navigate('Login')}
-              style={{color: '#20A8F4'}}>
-              Sign in
+          <View style={{paddingHorizontal: 24}}>
+            <PrimaryInput height={40} placeholder="First Name" />
+            <Spacer bottom={24} />
+            <PrimaryInput height={40} placeholder="Last Name" />
+            <Spacer bottom={24} />
+            <PrimaryInput height={40} placeholder="Email" />
+            <Spacer bottom={24} />
+            <PrimaryInput height={40} placeholder="Contact number" />
+            <Spacer bottom={24} />
+            <PrimaryInput height={40} placeholder="Password" />
+            <Spacer bottom={24} />
+            <PrimaryInput height={40} placeholder="Confirm password" />
+            <Spacer bottom={24} />
+
+            <PrimaryButton
+              color={'#254A7C'}
+              onPress={() => navigation.navigate('LandingStacks')}
+              title="Sign Up"
+              txtStyle={{color: theme.white}}
+            />
+            <Spacer bottom={24} />
+            <Text style={{textAlign: 'center', color: theme.white}}>
+              Already have an account?{' '}
+              <Text
+                onPress={() => navigation.navigate('Login')}
+                style={{color: '#20A8F4'}}>
+                Sign in!
+              </Text>
             </Text>
-            !
-          </Text>
+            <Spacer bottom={48} />
+            <Text
+              style={{textAlign: 'center', color: theme.white, fontSize: 12}}>
+              By using this service, you confirm that you have read, understood
+              and that you accept our{' '}
+              <Text style={{color: '#20A8F4'}}>Terms and Conditions</Text>
+            </Text>
+          </View>
         </View>
       </View>
     </ScrollView>
