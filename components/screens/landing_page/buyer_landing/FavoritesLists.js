2846c7a31ac7a98bb2c68b2f882b98daf08211e2
danmark.quinones <danmark.quinones@pointwest.com.ph>
2021-11-29 20:30:54 +0800

    setup api call in landing page

--- components/screens/landing_page/buyer_landing/FavoritesLists.js
+++ components/screens/landing_page/buyer_landing/FavoritesLists.js
@@ -1,18 +1,39 @@
 import React , {useEffect , useState} from 'react'
-import { FlatList } from 'react-native'
+import { FlatList , Text} from 'react-native'
 import { SquareCard, WhiteCard } from '../../../custom_components/customCards'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { theme } from '../../../contants/colors';
 import { cars } from '../../../contants/dummyCarData';
+import { fetchCars } from '../../../store/api_calls/cars_api';
 
 const FavoritesLists = (props) => {
 
     const {navigation , handleSeeMore} = props 
 
+    const [data , setData] = useState([])
+    const [isLoading , setIsLoading] = useState(false)
+
+    useEffect(() => {
+        setIsLoading(true)
+        const getCars = fetchCars()
+        getCars.then((res)=>{
+            if(res.data){
+                setData(cars)
+                setIsLoading(false)
+            }
+        }).catch((e)=>{
+            setIsLoading(false)
+        })
+    }, [])
+
     return (
-        <FlatList
+        isLoading?
+            <Text>Loading</Text>
+        :!isLoading&&data.length===0?
+            <Text>No Data</Text>
+        :<FlatList
             horizontal
-            data={cars}
+            data={data}
             keyExtractor={(item) => item.id}
             showsHorizontalScrollIndicator={false}
             renderItem={({item , index})=>(
