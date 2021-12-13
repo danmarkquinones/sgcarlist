3063ba8290b273e8f558ba6620f76b92a5360f02
Mark Kristian Oco <mark.oco.ext@axa.com.ph>
2021-12-14 01:49:15 +0800

    fix error

--- components/screens/landing_page/buyer_landing/TopDealersLists.js
+++ components/screens/landing_page/buyer_landing/TopDealersLists.js
@@ -6,73 +6,80 @@ import { SkeletonSquareCard } from '../../../custom_components/customCardLoaders
 import { FetchFailed } from '../../../custom_components/customFallbacks';
 
 const TopDealersLists = props => {
-  const {navigation} = props;
+  const {navigation, refreshing} = props;
+  const [dealers, setDealers] = useState([]);
 
-    const {navigation , refreshing} = props
-    const [dealers , setDealers] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [isLoading , setIsLoading] = useState(false)
+  const fetchData = () => {
+    setIsLoading(true);
+    const getLocations = fetchLandingPageLists('featured-dealers');
+    getLocations
+      .then(res => {
+        if (res.data) {
+          const displayDealers = res.data.data;
+          setDealers(displayDealers);
+          setIsLoading(false);
+        }
+      })
+      .catch(e => {
+        console.log('call failed', e);
+        setIsLoading(false);
+      });
+  };
 
-    const fetchData = () => {
-        setIsLoading(true)
-        const getLocations = fetchLandingPageLists('featured-dealers')
-        getLocations.then((res)=>{
-            if(res.data){
-                const displayDealers = res.data.data
-                setDealers(displayDealers)
-                setIsLoading(false)
-            }
-        }).catch((e)=>{
-            console.log('call failed' , e)
-            setIsLoading(false)
-        })
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (refreshing) {
+      fetchData();
     }
+  }, [refreshing]);
 
-    useEffect(() => {
-        fetchData()
-    }, [])
-
-    useEffect(() => {
-        if(refreshing){
-            fetchData()
-        }
-    }, [refreshing])
-
-    return (
-        <>
-        {isLoading?
-                <FlatList
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                data={[...Array(3)]}
-                keyExtractor={(item,index)=>index}
-                renderItem={({item})=>
-                    <SkeletonSquareCard width={150} height={150} borderRadius={5} marginLeft={10}/>
-                }
+  return (
+    <>
+      {isLoading ? (
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={[...Array(3)]}
+          keyExtractor={(item, index) => index}
+          renderItem={({item}) => (
+            <SkeletonSquareCard
+              width={150}
+              height={150}
+              borderRadius={5}
+              marginLeft={10}
             />
-        :!isLoading&&dealers.length===0?
-                <FetchFailed message="There seems to be a problem getting your request, please try again later"/>
-        :<FlatList
-            horizontal
-            data={dealers}
-            keyExtractor={(item) => item.id}
-            showsHorizontalScrollIndicator={false}
-            renderItem={({item , index})=>(
-                index!==3 ?
-                <DealerCard 
-                    dealer={item}
-                    onPress={()=>navigation.navigate('SellerView' , item)}
-                />
-                :<WhiteCard 
-                    options={{width:150}}
-                    onPress={()=>navigation.navigate('SearchResult')}
-                />
-            )}
+          )}
         />
-        }
-        </>
-        
-    )
-}
+      ) : !isLoading && dealers.length === 0 ? (
+        <FetchFailed message="There seems to be a problem getting your request, please try again later" />
+      ) : (
+        <FlatList
+          horizontal
+          data={dealers}
+          keyExtractor={item => item.id}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({item, index}) =>
+            index !== 3 ? (
+              <DealerCard
+                dealer={item}
+                onPress={() => navigation.navigate('SellerView', item)}
+              />
+            ) : (
+              <WhiteCard
+                options={{width: 150}}
+                onPress={() => navigation.navigate('SearchResult')}
+              />
+            )
+          }
+        />
+      )}
+    </>
+  );
+};
 
 export default TopDealersLists
--- components/screens/my_ads_page/MyAdsIndex.js
+++ components/screens/my_ads_page/MyAdsIndex.js
@@ -20,45 +20,61 @@ const MyAdsIndex = () => {
     listCars: cars,
   });
 
-  return (
-    <View style={{flex: 1}}>
-      <CustomHeader
-        canGoBack={false}
-        titleStyle={{
-          fontSize: scaleFont(20),
-          color: '#fff',
-          fontWeight: 'bold',
-          textAlign: 'center',
-        }}
-        title="SGCARLIST"
-        isTitleCenter
-      />
+    const [sortBy, setSortBy] = useState({
+      sort: 'asc-price',
+      height: 250,
+      options: [
+        {value: 'asc-price', label: 'Price - Lowest'},
+        {value: 'desc-price', label: 'Price - Highest'},
+        {value: 'asc-date-posted', label: 'Date - Newest'},
+        {value: 'desc-date-posted', label: 'Date - Oldest'},
+        {value: 'relevancy', label: 'By Relevance'},
+      ],
+    });
 
-      <SorterComponent config={config} setConfig={setConfig} />
-
+    return (
       <View style={{flex: 1}}>
-        <Spacer bottom={16} />
-        <View style={{width: '90%', alignSelf: 'center'}}>
-          <PrimaryInput placeholder="Search your listed car" />
-        </View>
-        <Spacer bottom={16} />
-        <FlatList
-          contentContainerStyle={{alignItems: 'center'}}
-          data={cars}
-          keyExtractor={item => item.id}
-          showsHorizontalScrollIndicator={false}
-          renderItem={({item, index}) => (
-            <ListCard
-              onPress={() => navigation.navigate('ProductView', item)}
-              car={item}
-              Icon={() => <Icon name="kebab-horizontal" size={20} solid />}
-              sellerMode={true}
-            />
-          )}
+        <CustomHeader
+          canGoBack={false}
+          titleStyle={{
+            fontSize: scaleFont(20),
+            color: '#fff',
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }}
+          title="SGCARLIST"
+          isTitleCenter
         />
+
+        <SorterComponent
+          sortBy={sortBy}
+          config={config}
+          setConfig={setConfig}
+        />
+
+        <View style={{flex: 1}}>
+          <Spacer bottom={16} />
+          <View style={{width: '90%', alignSelf: 'center'}}>
+            <PrimaryInput placeholder="Search your listed car" />
+          </View>
+          <Spacer bottom={16} />
+          <FlatList
+            contentContainerStyle={{alignItems: 'center'}}
+            data={cars}
+            keyExtractor={item => item.id}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({item, index}) => (
+              <ListCard
+                onPress={() => navigation.navigate('ProductView', item)}
+                car={item}
+                Icon={() => <Icon name="kebab-horizontal" size={20} solid />}
+                sellerMode={true}
+              />
+            )}
+          />
+        </View>
       </View>
-    </View>
-  );
+    );
 };
 
 export default MyAdsIndex;
