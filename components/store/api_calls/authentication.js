94baad4a83e9626bb168716620954c1830bd9895
Mark Kristian Oco <mark.oco.ext@axa.com.ph>
2021-12-14 02:56:07 +0800

    delete api

--- components/store/api_calls/authentication.js
+++ components/store/api_calls/authentication.js
@@ -57,6 +57,23 @@ export const getAdvert = (route, params) => {
     });
 };
 
+export const deleteProduct = (route, params) => {
+  const token = Kjur.encode(params);
+
+  console.log(token);
+
+  return axios
+    .delete(`${API_BASE_URL}${route}?token=${token}`)
+    .then(res => {
+      console.log('DELETED', res);
+      return res;
+    })
+    .catch(error => {
+      console.log('DELETE ERROR', error);
+      return {error: error.message, status: 500};
+    });
+};
+
 
 
 export const logout = () => {
