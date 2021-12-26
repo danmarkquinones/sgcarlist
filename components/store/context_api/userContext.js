a23ba417f42bd166067aaa211c1e274582c0222a
Mark Kristian Oco <mark.oco.ext@axa.com.ph>
2021-12-27 06:50:04 +0800

    update userDetails

--- components/store/context_api/userContext.js
+++ components/store/context_api/userContext.js
@@ -6,35 +6,50 @@ import { getAsyncStorageData } from '../api_calls/user_api';
 export const UserConfigContext = React.createContext();
 
 export const UserConfigContextProvider = (props) => {
+  const [userConfig, setUserConfig] = useState({
+    isLaunched: 0,
+    isLoggedIn: 0,
+    isSellMode: 0,
+    isNotificationOn: 0,
+    language: 'en',
+    userDetails: {},
+  });
 
-    const [userConfig , setUserConfig] = useState({
-        isLaunched : 0,
-        isLoggedIn : 0,
-        isSellMode : 0,
-        isNotificationOn : 0,
-        language:'en',
-        userDetails : {},
-    });
-    
-    useEffect(()=>{
-        const userInfo = getAsyncStorageData('userDetails' , '{}')
-        getAsyncStorageData('language','en')
-        userInfo.then((data)=>{
-            if(Object.keys(data)>0){
-                // console.log('login',data)
-                setUserConfig({...userConfig ,isLoggedIn:1,  userDetails:JSON.parse(data)})
-            }else{
-                // console.log('logout')
-                setUserConfig({...userConfig ,isLoggedIn:0,  userDetails:{}})
-            }
-        })
-        
-        LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
-    },[])
+  const getUserInfo = async () => {
+    try {
+      let value = await AsyncStorage.getItem('userDetails');
+      if (value !== null) {
+        // We have data!!
+        value = JSON.parse(value);
+        console.log(value);
+        if (Object.keys(value).length > 0) {
+          setUserConfig({
+            ...userConfig,
+            isLoggedIn: 1,
+            userDetails: value,
+          });
+        } else {
+          setUserConfig({...userConfig, isLoggedIn: 0, userDetails: {}});
+        }
+      } else {
+        setUserConfig({...userConfig, isLoggedIn: 0, userDetails: {}});
+      }
+    } catch (error) {
+      console.log(error);
+      // Error retrieving data
+    }
+  };
 
-    return (
-        <UserConfigContext.Provider value={[userConfig , setUserConfig]}>
-            {props.children}
-        </UserConfigContext.Provider>
-    )
+  useEffect(() => {
+    getUserInfo();
+    getAsyncStorageData('language', 'en');
+
+    LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
+  }, []);
+
+  return (
+    <UserConfigContext.Provider value={[userConfig, setUserConfig]}>
+      {props.children}
+    </UserConfigContext.Provider>
+  );
 }
